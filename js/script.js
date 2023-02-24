9e111fda813752df128ef225c7a1ccff42232a75
sollendy <edlannino@tutanota.com>
2023-02-24 15:14:26 +0100

    aggiunto leggero bootstrap

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 // creare una variabile che rappresenti la lista 
-let buzzinnerEl = document.getElementById(buzzList);
+let buzzinnerEl = document.getElementById("buzzList");
 //creo quello che dovrebbe essere il pattern del ciclo
-for (let i = 1; i <= 100; i++)
+for (let i = 1; i <= 100; i++) {
+    //devo inserire nel HTML un contenitore degli elementi della lista
+    let altroDiv = document.createElement('div');
+
+    //adesso bisogna inserirlo dentro buzzList
+    buzzinnerEl.append(altroDiv);
+
+    //dare un valore all'interno di altroDiv
+    altroDiv.innerHTML = "fizz " + (i);
+}
