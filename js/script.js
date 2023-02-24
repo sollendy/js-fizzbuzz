89cd636f5e4a7290a36747a9770491c24b7e11f2
sollendy <edlannino@tutanota.com>
2023-02-24 14:45:06 +0100

    esercizio in corso

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// creare una variabile che rappresenti la lista 
+let buzzinnerEl = document.getElementById(buzzList);
+//creo quello che dovrebbe essere il pattern del ciclo
+for (let i = 1; i <= 100; i++)
